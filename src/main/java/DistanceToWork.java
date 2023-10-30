c99396c62dd3b849fe3fa1426c3218658c1efb4d
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-30 21:54:16 +0400

    добавление неизменяемых переменных

--- src/main/java/DistanceToWork.java
+++ src/main/java/DistanceToWork.java
@@ -1,16 +1,16 @@
 public class DistanceToWork {
     public static void main(String[] args) {
-        int distanceFromHomeToOffice = 15; //расстояние от дома до офиса
+        final int Distance_From_Home_To_Office = 15; //расстояние от дома до офиса
         double averageSpeedTaxi = 60.0;
-        double eveningSpeedReduction = 1.5; // коэффицент снижения скорости транспорта вечером
-        double testerTravelTimeToOffice = distanceFromHomeToOffice / averageSpeedTaxi;  //время в пути тестировщика в офис из дома
-        double testerTravelTimeToHome = testerTravelTimeToOffice * eveningSpeedReduction;
+        final double Evening_Speed_Reduction = 1.5; // коэффицент снижения скорости транспорта вечером
+        double testerTravelTimeToOffice = Distance_From_Home_To_Office / averageSpeedTaxi;  //время в пути тестировщика в офис из дома
+        double testerTravelTimeToHome = testerTravelTimeToOffice * Evening_Speed_Reduction;
         float testerTravelTimeToOfficeFloat = (float) testerTravelTimeToOffice; //приведение данных к типу float
         float testerTravelTimeToHomeFloat = (float) testerTravelTimeToHome;
 
-        System.out.println("Расстояние от дома до работы - " + distanceFromHomeToOffice + " км.");
+        System.out.println("Расстояние от дома до работы - " + Distance_From_Home_To_Office + " км.");
         System.out.println("Средняя скорость такси - " + averageSpeedTaxi + " км/ч.");
-        System.out.println("Коэффицент снижения скорости транспорта в вечернее время на - " + eveningSpeedReduction);
+        System.out.println("Коэффицент снижения скорости транспорта в вечернее время на - " + Evening_Speed_Reduction);
         System.out.println("Время в дороге от дома к офису - " + testerTravelTimeToOfficeFloat + " часа.");
         System.out.println("Время пути тестировщика с работы до дома - " + testerTravelTimeToHomeFloat + " часа.");
     }
