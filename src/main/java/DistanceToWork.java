e27961f6c651b0b7616a2692365c9471342f4f77
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-30 21:11:08 +0400

    add task 7

---
+++ src/main/java/DistanceToWork.java
@@ -0,0 +1,17 @@
+public class DistanceToWork {
+    public static void main(String[] args) {
+        int distanceFromHomeToOffice = 15; //расстояние от дома до офиса
+        double averageSpeedTaxi = 60.0;
+        double eveningSpeedReduction = 1.5; // коэффицент снижения скорости транспорта вечером
+        double testerTravelTimeToOffice = distanceFromHomeToOffice / averageSpeedTaxi;  //время в пути тестировщика в офис из дома
+        double testerTravelTimeToHome = testerTravelTimeToOffice * eveningSpeedReduction;
+        float testerTravelTimeToOfficeFloat = (float) testerTravelTimeToOffice; //приведение данных к типу float
+        float testerTravelTimeToHomeFloat = (float) testerTravelTimeToHome;
+
+        System.out.println("Расстояние от дома до работы - " + distanceFromHomeToOffice + " км.");
+        System.out.println("Средняя скорость такси - " + averageSpeedTaxi + " км/ч.");
+        System.out.println("Коэффицент снижения скорости транспорта в вечернее время на - " + eveningSpeedReduction);
+        System.out.println("Время в дороге от дома к офису - " + testerTravelTimeToOfficeFloat + " часа.");
+        System.out.println("Время пути тестировщика с работы до дома - " + testerTravelTimeToHomeFloat + " часа.");
+    }
+}
